b71ea70bb39932a99d603f0817bd8e0b235d1415
kumar_000 <leelaparasa@gmail.com>
2017-03-16 13:58:03 -0700

    Initial commit

---
+++ src/js/magic.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kumar_000 on 3/15/2017.
+ */
+
+var hello = 'Look Im working on grunting';
+var awesome = 'Yes it is awesome to use grunt';
+var greet = 'Hello All.';
+var wish = 'Happy coding.';
+var greetings = 'Have a good day.';
+
